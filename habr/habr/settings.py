cf79e776bb470c4a5cdaffbc789762b10a5a9e7a
truart <trubinart@gmail.com>
2022-01-06 13:09:20 +0300

    add article app

--- habr/habr/settings.py
+++ habr/habr/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
 
     'authapp',
 ]
@@ -123,5 +124,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'authapp.User'
