b9f1ba4a26969d4f22fb0f72c165d1f4ced20bd7
truart <trubinart@gmail.com>
2022-01-19 13:28:19 +0300

    1. feat(filling_database.py): correct code in according to database change 2. fix(index.html): delete commented out code 3. fix(mainapp/models.py): add ForeignKey to user filed in ArticleComment

--- habr/mainapp/management/commands/filling_database.py
+++ habr/mainapp/management/commands/filling_database.py
@@ -11,7 +11,7 @@ from django.core.files import File
 from mainapp.models import ArticleCategories, Article, ArticleLike, ArticleComment
 from authapp.models import User
 
-# from mainapp.models import Article, ArticleLike, ArticleComment
+from mainapp.models import Article, ArticleLike, ArticleComment
 from authapp.models import UserProfile
 
 
@@ -28,31 +28,38 @@ class Command(BaseCommand):
         birthday = Datetime()
 
         print('Заполняю таблицу USERS')
+        for _ in range(10):
+            user = User(
+                username=person.username(mask='C'),
+                email=person.email(domains=None, unique=True),
+                password=person.password(length=8))
 
-        for _ in range(30):
-            username = person.username(mask='C')
-            user = UserProfile(
-                first_name=person.first_name(gender=None),
-                last_name=person.last_name(gender=None),
-                username=username,
-                # email=person.email(domains=None, unique=True),
-                # password=person.password(length=8),
-                birthday=birthday.formatted_datetime(fmt="%Y-%m-%d"))
+            user.save()
 
-            img_url = Internet().stock_image(width=100, height=100, keywords=['люди'])
+        print('Заполняю таблицу USERS PROFILE')
+        for item in User.objects.all():
+            name = person.name()
+            user_profile = UserProfile(
+                user = item,
+                name = name,
+                birthday = birthday.formatted_datetime(fmt="%Y-%m-%d"),
+                bio = text.text(quantity=1))
 
+
+            img_url = Internet().stock_image(width=50, height=50, keywords=['лицо'])
             img_file = requests.get(img_url)
 
-            file_name = f'{username}.png'
+            file_name = f'{name}.png'
             with open(file_name, 'wb') as file:
                 file.write(img_file.content)
 
             with open(file_name, 'rb') as file:
                 data = File(file)
-                user.avatar.save(file_name, data, True)
+                user_profile.avatar.save(file_name, data, True)
 
             os.remove(file_name)
-            user.save()
+            user_profile.save()
+
 
         # CREATE ARTICLE CATEGORIES
         ARTICLE_CATEGORIES = ['Дизайн', 'Веб-разработка', 'Мобильная разработка', 'Маркетинг']
@@ -64,20 +71,19 @@ class Command(BaseCommand):
 
         # CREATE ARTICLES
         print('Заполняю таблицу ARTICLES')
-        for i in range(30):
+        for i in range(10):
             # create article
             new_article = Article(
-                title=text.title(),
-                subtitle=text.sentence(),
+                title=" ".join([text.word() for i in range(0, 5)]),
+                subtitle=" ".join([text.word() for i in range(0, 10)]),
                 text=text.text(quantity=15))
             # set categories
             new_article.categories = random.choice(ArticleCategories.objects.all())
             # set author
-            new_article.user = random.choice(UserProfile.objects.all())
+            new_article.user = random.choice(User.objects.all())
 
             # create and set article images
-            # TODO указать размер картинки для статей
-            img_url = Internet().stock_image(width=1920, height=1080, keywords=['природа'])
+            img_url = Internet().stock_image(width=390, height=300, keywords=['природа'])
             img_file = requests.get(img_url)
 
             file_name = f'{text.word()}.png'
@@ -95,16 +101,18 @@ class Command(BaseCommand):
 
         # CREATE LIKES
         print('Заполняю таблицу LIKES')
-        for item in UserProfile.objects.all():
-            new_like = ArticleLike(like=True)  # вот здесь указывает на ошибку лишнего аргумента
+        for item in User.objects.all():
+            new_like = ArticleLike(like=True)
             new_like.article_like = random.choice(Article.objects.all())
             new_like.user = item
             new_like.save()
 
         # CREATE COMMENTS
         print('Заполняю таблицу COMMENTS')
-        for item in UserProfile.objects.all():
-            new_comment = ArticleComment(text=text.text(quantity=2))
-            new_comment.article_comment = random.choice(Article.objects.all())
-            new_comment.user = item
-            new_comment.save()
+        for item in User.objects.all():
+            count = random.choice([i for i in range(0,10)])
+            for i in range(0, count):
+                new_comment = ArticleComment(text=text.text(quantity=2))
+                new_comment.article_comment = random.choice(Article.objects.all())
+                new_comment.user = item
+                new_comment.save()
