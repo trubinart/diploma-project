1232388a694b70738fb0930aaf6d1161d9ea3e27
Yuriy IV <thesand2009@yandex.ru>
2022-01-23 12:41:58 +0300

    1.fix(authapp/models.py): del field "name" in "UserProfile" 2.feat: created authorization process 3. fix: changes made urls, views, templates and style sheets

--- habr/habr/urls.py
+++ habr/habr/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from mainapp.views import MainListView, LkListView, ArticleDetailView
-from authapp.views import UserRegistrationView
 
 urlpatterns = [
 
     path('', MainListView.as_view(), name='main'),
     path('lk/', LkListView.as_view(), name='lk'),
     path('article/<str:pk>/', ArticleDetailView.as_view(), name='article'),
-    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('auth/', include('authapp.urls', namespace='auth')),
 
     path('admin/', admin.site.urls),
 ]
