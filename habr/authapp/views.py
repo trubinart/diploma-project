33643e48bb1bd787cbb8e34c2d1344ba9dceb50c
Yuriy IV <thesand2009@yandex.ru>
2022-01-24 20:51:07 +0300

    fix: article_page.html, models.py, views.py, deleted old and added new migrations

--- habr/authapp/views.py
+++ habr/authapp/views.py
@@ -4,6 +4,10 @@ from django.views.generic import CreateView
 
 from authapp.forms import UserRegisterForm, UserLoginForm
 from authapp.models import User
+from mainapp.models import ArticleCategories
+
+"""обозначение списка категорий для вывода в меню во разных view"""
+category_list = ArticleCategories.objects.all()
 
 
 class UserRegistrationView(CreateView):
@@ -16,6 +20,7 @@ class UserRegistrationView(CreateView):
         context = super().get_context_data(**kwargs)
         title = 'Регистрация'
         context['title'] = title
+        context['categories_list'] = category_list
         return context
 
 
@@ -27,6 +32,7 @@ class UserLoginView(LoginView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Авторизация'
+        context['categories_list'] = category_list
         return context
 
 
