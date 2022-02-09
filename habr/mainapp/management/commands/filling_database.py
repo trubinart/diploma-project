bf0d4c19e323d5fbc09001e0a36ad581bf15f316
truart <trubinart@gmail.com>
2022-02-09 14:25:24 +0300

    fix: generate tags in filling_database.py

--- habr/mainapp/management/commands/filling_database.py
+++ habr/mainapp/management/commands/filling_database.py
@@ -86,10 +86,6 @@ class Command(BaseCommand):
             new_article.subtitle = f'Падзаголовок статьи #{i} автора {new_article.user.username}'
             new_article.text = text.text(quantity=15)
 
-            new_article.tags.add(*[text.word() for i in range(4)])
-
-            new_article.tatags.add('')
-
             # create and set article images
             img_url = Internet().stock_image(width=390, height=300, keywords=['природа'])
             img_file = requests.get(img_url)
@@ -113,6 +109,7 @@ class Command(BaseCommand):
 
             # save article
             new_article.save()
+            new_article.tags.add(*[text.word() for i in range(4)])
 
         # CREATE COMMENTS
         print('Заполняю таблицу COMMENTS')
